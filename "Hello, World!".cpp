12adaa39c2e87e72b49c20ff0d6fd35535708e52
EgorChernishevskiy <100187552+EgorChernishevskiy@users.noreply.github.com>
2022-03-03 15:06:56 +0400

    Исправил программу программу так, чтобы последовательность чисел начиналась со значений (0, 1, ...).

--- "Hello, World!".cpp
+++ "Hello, World!".cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int fib(int n)
 {
-    int a = 1;
+    int a = 0;
     int b = 1;
-    for (int i = 0; i < n - 2; i++)
+    for (int i = 0; i < n - 1; i++)
     {
         a = a + b;
         b = a - b;
