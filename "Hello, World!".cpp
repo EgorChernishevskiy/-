a065b6e7384483f601a26a5fb7a253d71d60470b
EgorChernishevskiy <100187552+EgorChernishevskiy@users.noreply.github.com>
2022-03-03 15:02:40 +0400

    Дополнил программу, добавив в нее функцию,  которая будет рекурсивно вычислять n-ое число Фибоначчи (последовательность (1, 1, ...))

--- "Hello, World!".cpp
+++ "Hello, World!".cpp
@@ -2,8 +2,26 @@
 
 using namespace std;
 
+int fib(int n)
+{
+    int a = 1;
+    int b = 1;
+    for (int i = 0; i < n - 2; i++)
+    {
+        a = a + b;
+        b = a - b;
+    }
+    return a;
+}
+
 int main() 
 { 
     cout << "Hello, world!" << endl;
+    
+    int n;
+    cout << "n="; cin >> n;
+    cout << fib(n) << " ";
+    cout << endl;
+    
     return 0; 
 }
